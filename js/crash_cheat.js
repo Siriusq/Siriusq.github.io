09879266c19d7a9e63b332d1f0c34478a7566f28
Siriusq <eargon.skyle@gmail.com>
2023-10-23 21:11:16 +0800

    Site updated: 2023-10-23 21:11:14

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,15 +1,27 @@
 var OriginTitle = document.title;
 var titleTime;
+var crashedIcon;
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    crashedIcon = "/images/crashed-dark.png";
+} else {
+    crashedIcon = "/images/crashed.png";
+}
+
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        var icon = document.querySelector('[rel="icon"]');
-        icon.setAttribute('href', "/images/crashed.png");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        var icons = document.querySelectorAll("link[rel='icon']");
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].href = crashedIcon;
+        }
+        document.title = '╭(°A°`)╮ 页面崩溃啦~';
         clearTimeout(titleTime);
     } else {
-        var icon = document.querySelector('[rel="icon"]');
-        icon.setAttribute('href', "/images/favicon.png");
-        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+        var icons = document.querySelectorAll("link[rel='icon']");
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].href = "/images/favicon.png";
+        }
+        document.title = '(ฅ>ω<*ฅ) 诶嘿，又好了~ ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
